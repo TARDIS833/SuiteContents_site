b4a98b4f0ec28e19717280237ffb6f6dce1ce240
Heniinmay <realhayley05@gmail.com>
2022-10-07 01:27:05 +0900

    fix: Error, Hydration failed 해결

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,49 @@
 // pages/_document.tsx
+import createEmotionServer from '@emotion/server/create-instance'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import createEmotionCache from 'src/utils/create-emotion-cache'
 export default class CustomDocument extends Document {
   render() {
     return (
       <Html lang="kr">
-        <Head></Head>
+        <Head>
+          {/* Font made from <a href="http://www.onlinewebfonts.com">oNline Web Fonts</a>is licensed by CC BY 3.0 */}
+        </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
     )
+  }
+}
+
+CustomDocument.getInitialProps = async (ctx) => {
+  const originalRenderPage = ctx.renderPage
+  const cache = createEmotionCache()
+  const { extractCriticalToChunks } = createEmotionServer(cache)
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App: any) =>
+        function EnhanceApp(props) {
+          return <App emotionCache={cache} {...props} />
+        },
+    })
+
+  const initialProps = await Document.getInitialProps(ctx)
+  const emotionStyles = extractCriticalToChunks(initialProps.html)
+  const emotionStyleTags = emotionStyles.styles.map((style) => (
+    <style
+      data-emotion={`${style.key} ${style.ids.join(' ')}`}
+      key={style.key}
+      // eslint-disable-next-line react/no-danger
+      dangerouslySetInnerHTML={{ __html: style.css }}
+    />
+  ))
+
+  return {
+    ...initialProps,
+    emotionStyleTags,
   }
 }
