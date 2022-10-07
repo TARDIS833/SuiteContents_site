a24229c6f7bb93327dea551b4eb6a0c207ac1450
Heniinmay <realhayley05@gmail.com>
2022-10-08 01:06:31 +0900

    fix: layout bug fix 2

---
+++ src/components/layout/default-layout.tsx
@@ -0,0 +1,7 @@
+import { DefaultLayoutProps } from './types'
+
+export function DefautlLayout(props: DefaultLayoutProps) {
+  const { children }: any = props
+  return <>{children}</>
+}
+export default DefautlLayout
